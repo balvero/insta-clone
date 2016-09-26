84471f7facd8454c61104395b9b88c2ee8510212
Bernard Alvero <bernard.alvero@gmail.com>
2016-09-26 15:21:53 +0800

    Create show and index action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,12 @@
 class PhotosController < ApplicationController
+  before_action :find_photo, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @photos = Photo.all.order('created_at DESC')
+  end
+
+  def show
+
   end
 
   def new
@@ -10,12 +15,23 @@ class PhotosController < ApplicationController
 
   def create
     @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo, notice: "Photo has been posted!"
+    else
+      render 'new'
+    end
   end
+
 
   private
 
   def photo_params
     params.require(:photo).permit(:title, :description)
+  end
+
+  def find_photo
+    @photo = Photo.find(params[:id])
   end
 
 end
