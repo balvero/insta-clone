16830f2b2105421c416c238a6c5ee54d649a9ae6
Bernard Alvero <bernard.alvero@gmail.com>
2016-09-26 14:42:44 +0800

    Create Photo actions and views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,19 @@ class PhotosController < ApplicationController
   def index
     
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description)
+  end
+
 end
