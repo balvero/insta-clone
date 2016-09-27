a7ed43e2c795393146b31dea4f9aca238aa77057
Bernard Alvero <bernard.alvero@gmail.com>
2016-09-27 11:01:02 +0800

    Only current user can edit/delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   before_action :find_photo, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @photos = Photo.all.order('created_at DESC')
@@ -52,6 +53,13 @@ class PhotosController < ApplicationController
 
   def find_photo
     @photo = Photo.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @photo.user
+      flash[:danger] = 'You can only edit or delete your own photos.'
+      redirect_to root_path
+    end
   end
 
 end
