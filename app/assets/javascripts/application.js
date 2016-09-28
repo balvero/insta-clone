edfa8026cddc8a41dd952e6ceba03c1c98ab860b
Bernard Alvero <bernard.alvero@gmail.com>
2016-09-28 18:17:45 +0800

    js and css cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require bootstrap-sprockets
 //= require masonry/jquery.masonry
-//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
 
