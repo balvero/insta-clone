7cc2724a94ea8f029a7cfad32938c755f257e4c2
Bernard Alvero <bernard.alvero@gmail.com>
2016-09-29 20:17:36 +0800

    Added validation and edited css.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   acts_as_votable
   belongs_to :user
+  validates_presence_of :image
+  validates :title, presence: true, length: { minimum: 3, maximum: 120 }
+  validates :description, presence: true, length: { minimum: 3 }
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
